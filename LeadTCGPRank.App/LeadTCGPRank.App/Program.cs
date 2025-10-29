5c54ad1a5c50f872fd8c755a73230949631641e4
LaM0uette <lam0uette.dev@gmail.com>
2025-10-29 19:59:20 +0100

    first setup

---
+++ LeadTCGPRank.App/LeadTCGPRank.App/Program.cs
@@ -0,0 +1,32 @@
+using LeadTCGPRank.App.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(LeadTCGPRank.App.Client._Imports).Assembly);
+
+app.Run();
