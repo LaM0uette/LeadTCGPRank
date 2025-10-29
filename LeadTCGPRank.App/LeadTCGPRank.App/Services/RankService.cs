f7a442627741f59a81b0434d224d00ec0cbddab1
LaM0uette <lam0uette.dev@gmail.com>
2025-10-30 00:51:21 +0100

    set win and tie

--- LeadTCGPRank.App/LeadTCGPRank.App/Services/RankService.cs
+++ LeadTCGPRank.App/LeadTCGPRank.App/Services/RankService.cs
@@ -5,6 +5,9 @@ namespace LeadTCGPRank.App.Services;
 public class RankService : IRankService
 {
     #region Statements
+    
+    private const int _baseWinPoints = 10;
+    private readonly Dictionary<int, int> _winStreaksBonusPoints = new() { { 0, 0 }, { 1, 0 }, { 2, 3 }, { 3, 6 }, { 4, 9 }, { 5, 12 } };
 
     private readonly IStatsService _statsService;
 
@@ -19,22 +22,65 @@ public class RankService : IRankService
 
     public async Task<Stats> WinAsync(CancellationToken cancellationToken = default)
     {
-        int current = await _statsService.GetWinsAsync(cancellationToken);
-        await _statsService.SetWinsAsync(current + 1, cancellationToken);
+        // Update Wins
+        int currentWins = await _statsService.GetWinsAsync(cancellationToken);
+        currentWins++;
+        
+        await _statsService.SetWinsAsync(currentWins, cancellationToken);
+        
+        // Update Win Streaks
+        int currentWinStreaks = await _statsService.GetWinStreaksAsync(cancellationToken);
+        currentWinStreaks++;
+        
+        await _statsService.SetWinStreaksAsync(currentWinStreaks, cancellationToken);
+        
+        // Update Points
+        int currentPoints = await _statsService.GetPointsAsync(cancellationToken);
+        currentPoints += _baseWinPoints;
+        
+        if (_winStreaksBonusPoints.TryGetValue(currentWinStreaks, out int point))
+        {
+            currentPoints += point;
+        }
+        else if (currentWinStreaks > 5)
+        {
+            currentPoints += _winStreaksBonusPoints[5];
+        }
+        
+        await _statsService.SetPointsAsync(currentPoints, cancellationToken);
+        
         return await _statsService.GetAsync(cancellationToken);
     }
 
     public async Task<Stats> LooseAsync(CancellationToken cancellationToken = default)
     {
-        int current = await _statsService.GetLoosesAsync(cancellationToken);
-        await _statsService.SetLoosesAsync(current + 1, cancellationToken);
+        // Update Losses
+        int currentLosses = await _statsService.GetLoosesAsync(cancellationToken);
+        currentLosses++;
+        
+        await _statsService.SetLoosesAsync(currentLosses, cancellationToken);
+        
+        // Reset Win Streaks
+        await _statsService.SetWinStreaksAsync(0, cancellationToken);
+        
+        // Update Points
+        int currentPoints = await _statsService.GetPointsAsync(cancellationToken);
+        
+        
         return await _statsService.GetAsync(cancellationToken);
     }
 
     public async Task<Stats> TieAsync(CancellationToken cancellationToken = default)
     {
-        int current = await _statsService.GetTiesAsync(cancellationToken);
-        await _statsService.SetTiesAsync(current + 1, cancellationToken);
+        // Update Ties
+        int currentTies = await _statsService.GetTiesAsync(cancellationToken);
+        currentTies++;
+        
+        await _statsService.SetTiesAsync(currentTies, cancellationToken);
+        
+        // Reset Win Streaks
+        await _statsService.SetWinStreaksAsync(0, cancellationToken);
+        
         return await _statsService.GetAsync(cancellationToken);
     }
 
